e9a24aa7eb8cee78c3d0d87dfd7492000d5c0ad8
soyoon1 <20210841@duksung.ac.kr>
2024-07-15 18:53:09 +0900

    [수정] API 명세서에 따라 오류 응답 상태 코드 수정 및 response 값 수정 (2024.07.15)

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, request, jsonify
+import os
+
+# 환경 변수 설정 (oneDNN 옵션 비활성화)
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+# TensorFlow 로그 수준 설정
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
 
 # emotion_analysis_deepface.py 파일에서 analyze_emotion 함수 임포트
 from emotion_analysis_deepface import analyze_emotion_deepface
@@ -8,17 +16,18 @@ app = Flask(__name__)
 @app.route('/analyze-deepface', methods=['POST'])
 def analyze_deepface():
   if 'file' not in request.files:
-    return jsonify({'error': 'No file part'})
+    return jsonify({'error': 'No file part'}), 400
 
   file = request.files['file']
   if file.filename == '':
-    return jsonify({'error': 'No selected file'})
+    return jsonify({'error': 'No selected file'}), 400
 
   if file:
     # 파일을 읽어서 OpenCV 이미지로 변환
-    emotions = analyze_emotion_deepface(file)
-
-    return jsonify({'emotions': emotions})
+    result = analyze_emotion_deepface(file)
+    if 'error' in result:
+      return jsonify(result), 500
+    return jsonify(result)
 
 @app.route('/') # 서버 테스트
 def home():
--- emotion_analysis_deepface.py
+++ emotion_analysis_deepface.py
@@ -8,9 +8,25 @@ def analyze_emotion_deepface(file):
   image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
   try:
-    # DeepFace를 사용하여 표정 인식 (얼굴이 감지되지 않았을 때 error가 나지 않게 하는 방법: enforce_detection=False 추가)
+    # DeepFace를 사용하여 표정 인식 
+    # (얼굴이 감지되지 않았을 때 error가 나지 않게 하는 방법: enforce_detection=False 추가)
     results = DeepFace.analyze(image, actions=['emotion'])
-    return results[0]['emotion']
+    emotions = results[0]['emotion']
+
+    # 가장 우세한 감정을 검색
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    # 우세한 감정이 'happy', 'neutral' 중 하나라면 사용자가 어려움을 겪고 있지 않다고 판단
+    if dominant_emotion in ['happy', 'neutral']:
+      is_difficult = False   
+    else:
+      # 부정적인 감정 상태 목록
+      difficult_emotions = ['angry', 'disgust', 'fear', 'sad', 'surprise']
+      # 감정 상태 중 하나라도 부정적이라면 isDifficult를 True로 설정(임계값: 0.5)
+      is_difficult = any(emotions[emotion] > 0.5 for emotion in difficult_emotions)
+
+    return {"isDifficult": is_difficult, "dominantEmotion": dominant_emotion}
+  
   except ValueError as e:
     # 얼굴이 감지되지 않았을 때 예외 처리
     return {'error': str(e)}
